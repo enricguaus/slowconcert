f75955e99669b6f04fb8a48de9cd6fd15c10411e
enricguaus <eguaus@yahoo.es>
2015-11-22 08:33:14 +0100

    Splitting Credentials from code

---
+++ credentials.py
@@ -0,0 +1,4 @@
+credentials_consumer_keyflag    = "putYoutConsumerKeyFlagHere"
+credentials_consumer_secretflag = "putYoutConsumerSecretFlagHere"
+credentials_access_keyflag      = "putYourAccesKeyFlagHerre"
+credentials_access_secretflag   = "putYourAccessSecretFlahHere"
