e4e182585ead82db602215810b103a7375adccc2
enricguaus <eguaus@yahoo.es>
2016-08-04 19:30:38 +0200

    Nothing new in august. Just check

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-credentials_consumer_keyflag    = "5yGkcjJ71lfnRPyaA2JYyms9T"
-credentials_consumer_secretflag = "vtcKEiKoOQFgjPnpyOb25cSKxyWekp15TSIgoinfZLsROtUSVX"
-credentials_access_keyflag      = "3387624453-f46e3vTICtZREfl3NjitbsqAjSzHHwMpfjmcGPr"
-credentials_access_secretflag   = "Y1L9fY1bEomVi8zdowm0yV6OQutmB47zB4s4hoyphMHwd"
+credentials_consumer_keyflag    = "aaa"
+credentials_consumer_secretflag = "bbb"
+credentials_access_keyflag      = "ccc"
+credentials_access_secretflag   = "ddd"
